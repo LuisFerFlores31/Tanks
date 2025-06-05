09f807da279448878f0bbbd0b5f5fa9fe33dab7e
JoseA4265 <A01738341@tec.mx>
2025-06-05 13:01:49 -0600

    Funcionalidad de Bala

--- main.py
+++ main.py
@@ -1,89 +1,105 @@
 import pygame
 from pygame.locals import *
 
-# Cargamos las bibliotecas de OpenGL
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 import math
-
-# Se carga el archivo de la clase Cubo
 import sys
+
 sys.path.append('..')
 from Cubo import Cubo
-# Import obj loader
-from objloader import *
+from Bala import Bala
 
-screen_width = 1200
-screen_height = 800
-#vc para el obser.
-FOVY=60.0
-ZNEAR=1.0
-ZFAR=900.0
-#Variables para definir la posicion del observador
-#gluLookAt(EYE_X,EYE_Y,EYE_Z,CENTER_X,CENTER_Y,CENTER_Z,UP_X,UP_Y,UP_Z)
-EYE_X = 0.0 #Maybe este es para mover hacia atras
-EYE_Y = 15.0
-EYE_Z = 0.0
-CENTER_X=1.0
-CENTER_Y=15.0
-CENTER_Z=0.0
-UP_X=0
-UP_Y=1
-UP_Z=0
-#Variables para dibujar los ejes del sistema
-X_MIN=-500
-X_MAX=500
-Y_MIN=-500
-Y_MAX=500
-Z_MIN=-500
-Z_MAX=500
-#Dimension del plano
+
+screen_width  = 1200
+screen_height =  800
+
+FOVY   = 60.0
+ZNEAR  =  1.0
+ZFAR   = 900.0
+
+
+EYE_X, EYE_Y, EYE_Z = 0.0, 5.0, 0.0
+CENTER_X, CENTER_Y, CENTER_Z = 1.0, 5.0, 0.0
+UP_X, UP_Y, UP_Z = 0, 1, 0
+
 DimBoard = 200
-#Vector de direcc. del observador
-dir = [1.0, 0.0, 0.0]
+
+dir = [1.0, 0.0, 0.0] 
 theta = 0.0
-total_theta = 0.0  # Track total rotation
-col = 0
 
-pygame.init()
-
-#cubo = Cubo(DimBoard, 1.0)
 cubos = []
 ncubos = 50
-# Guardo un cubo como el jugador para las colisiones
+
 player = Cubo(DimBoard, 1.0, 5.0)
-# Lista para almacenar los objetos 3D
-objetos = []
+
+bullets = []
+
+#de bala.py
+def shoot_bullet(bullets_list):
+    spawn_x = EYE_X + dir[0] * 2.0
+    spawn_y = EYE_Y
+    spawn_z = EYE_Z + dir[2] * 2.0
+    start_pos = [spawn_x, spawn_y, spawn_z]
+
+    new_bala = Bala(start_pos, [dir[0], 0.0, dir[2]], speed=3.0, size=1.0)
+    bullets_list.append(new_bala)
+
+def update_and_collide_bullets(bullets_list, cubos_list, board_limit):
+    cubos_to_remove = set()
+
+    for b in bullets_list:
+        if not b.alive:
+            continue
+
+        b.update()
+
+        b.check_wall_collision(board_limit)
+
+        if b.alive:
+            idx = b.check_cube_collision(cubos_list)
+            if idx >= 0:
+                cubos_to_remove.add(idx)
+
+    if cubos_to_remove:
+        new_cubos = []
+        for i, cube in enumerate(cubos_list):
+            if i not in cubos_to_remove:
+                new_cubos.append(cube)
+        cubos_list[:] = new_cubos  
+
+    bullets_list[:] = [b for b in bullets_list if b.alive]
+
 
 def Axis():
     glShadeModel(GL_FLAT)
     glLineWidth(3.0)
-    #X axis in red
-    glColor3f(1.0,0.0,0.0)
+    # Eje X
+    glColor3f(1.0, 0.0, 0.0)
     glBegin(GL_LINES)
-    glVertex3f(X_MIN,0.0,0.0)
-    glVertex3f(X_MAX,0.0,0.0)
+    glVertex3f(-DimBoard, 0.0, 0.0)
+    glVertex3f( DimBoard, 0.0, 0.0)
     glEnd()
-    #Y axis in green
-    glColor3f(0.0,1.0,0.0)
+    # Eje Y
+    glColor3f(0.0, 1.0, 0.0)
     glBegin(GL_LINES)
-    glVertex3f(0.0,Y_MIN,0.0)
-    glVertex3f(0.0,Y_MAX,0.0)
+    glVertex3f(0.0, -DimBoard, 0.0)
+    glVertex3f(0.0,  DimBoard, 0.0)
     glEnd()
-    #Z axis in blue
-    glColor3f(0.0,0.0,1.0)
+    # Eje Z
+    glColor3f(0.0, 0.0, 1.0)
     glBegin(GL_LINES)
-    glVertex3f(0.0,0.0,Z_MIN)
-    glVertex3f(0.0,0.0,Z_MAX)
+    glVertex3f(0.0, 0.0, -DimBoard)
+    glVertex3f(0.0, 0.0,  DimBoard)
     glEnd()
     glLineWidth(1.0)
 
 def Init():
     screen = pygame.display.set_mode(
         (screen_width, screen_height), DOUBLEBUF | OPENGL)
-    pygame.display.set_caption("OpenGL: cubos")
+    pygame.display.set_caption("OpenGL: Juego de Tanques")
 
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
@@ -91,117 +107,90 @@ def Init():
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    gluLookAt(EYE_X,EYE_Y,EYE_Z,CENTER_X,CENTER_Y,CENTER_Z,UP_X,UP_Y,UP_Z)
-    glClearColor(0,0,0,0)
+    gluLookAt(EYE_X, EYE_Y, EYE_Z,
+              CENTER_X, CENTER_Y, CENTER_Z,
+              UP_X,    UP_Y,    UP_Z)
+
+    glClearColor(0, 0, 0, 0)
     glEnable(GL_DEPTH_TEST)
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-    
+
     for i in range(ncubos):
-        cubos.append(Cubo(DimBoard, 1.0, 5.0))
-        
-    #se le pasan a los objetos el arreglo de cubos
+        c = Cubo(DimBoard, 1.0, 5.0)
+        cubos.append(c)
+
     for obj in cubos:
         obj.getCubos(cubos)
-    
-    # Configuración de iluminación
-    glLightfv(GL_LIGHT0, GL_POSITION,  (0, 200, 0, 0.0))
-    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.5, 0.5, 0.5, 1.0))
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
-    glEnable(GL_LIGHTING)
-    glEnable(GL_LIGHT0)
-    glEnable(GL_COLOR_MATERIAL)
-    glShadeModel(GL_SMOOTH)
-    
-    # Cargar el modelo del tanque
-    objetos.append(OBJ("Ejemplo11_objetos/swat_tank.obj", swapyz=True))
-    objetos[0].generate()
 
 def lookat():
-    global EYE_X
-    global EYE_Z
-    global CENTER_X
-    global CENTER_Z
-    global dir
-    global theta
-    dir_x = 0.0
-    dir_z = 0.0
+    global EYE_X, EYE_Z, CENTER_X, CENTER_Z, dir, theta
     rads = math.radians(theta)
-    dir_x = math.cos(rads)*dir[0] + math.sin(rads)*dir[2]
-    dir_z = -math.sin(rads)*dir[0] + math.cos(rads)*dir[2]
-    dir[0] = dir_x
-    dir[2] = dir_z
+    dir_x = math.cos(rads) * dir[0] + math.sin(rads) * dir[2]
+    dir_z = -math.sin(rads) * dir[0] + math.cos(rads) * dir[2]
+    dir[0], dir[2] = dir_x, dir_z
+
     CENTER_X = EYE_X + dir[0]
     CENTER_Z = EYE_Z + dir[2]
     glLoadIdentity()
-    gluLookAt(EYE_X, EYE_Y, EYE_Z, CENTER_X, CENTER_Y, CENTER_Z, UP_X, UP_Y, UP_Z)
-    
+    gluLookAt(EYE_X, EYE_Y, EYE_Z,
+              CENTER_X, CENTER_Y, CENTER_Z,
+              UP_X,    UP_Y,    UP_Z)
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     Axis()
-    #Se dibuja el plano gris
+    # Piso
     glColor3f(0.3, 0.3, 0.3)
     glBegin(GL_QUADS)
     glVertex3d(-DimBoard, 0, -DimBoard)
-    glVertex3d(-DimBoard, 0, DimBoard)
-    glVertex3d(DimBoard, 0, DimBoard)
-    glVertex3d(DimBoard, 0, -DimBoard)
+    glVertex3d(-DimBoard, 0,  DimBoard)
+    glVertex3d( DimBoard, 0,  DimBoard)
+    glVertex3d( DimBoard, 0, -DimBoard)
     glEnd()
-    #Se dibuja cubos
+
     for obj in cubos:
         obj.draw()
         obj.update()
-    
-    # Dibujar el tanque
-    glPushMatrix()
-    # Posicionar el tanque en la posición del jugador
-    glTranslatef(EYE_X, 0.0, EYE_Z)
-    # Rotar el tanque según la dirección total
-    glRotatef(total_theta, 0.0, 1.0, 0.0)
-    # Rotación inicial para orientar el modelo
-    glRotatef(-90.0, 1.0, 0.0, 0.0)
-    # Ajustar la escala del modelo (aumentado para hacerlo más visible)
-    glScale(5.0, 5.0, 5.0)
-    objetos[0].render()
-    glPopMatrix()
 
-done = False
+    for b in bullets:
+        b.draw()
+
+
+#Bucle principal
+pygame.init()
 Init()
+done = False
+
 while not done:
     keys = pygame.key.get_pressed()
-    #avanzar observador
-    if keys[pygame.K_UP]: #Cambiar Colisiones
-        # Calculo la nueva posicion y la guardo
+    
+    if keys[pygame.K_UP]:
         next_x = EYE_X + dir[0]
         next_z = EYE_Z + dir[2]
 
-         # Actualizo tanto la posicion como la direccion del jugador
         player.Position[0] = EYE_X
         player.Position[2] = EYE_Z
         player.Direction[0] = dir[0]
         player.Direction[2] = dir[2]
-        
-        # se checa colision con el mapa
+
         if not player.Detcol():
-            #Se checa la colision con todos los cubos y el jugador
             collision = False
             for cube in cubos:
                 if cube.checkPlayerCollision([next_x, EYE_Y, next_z]):
                     collision = True
                     break
-            
-            # Se mueve si no hay colision
+
             if not collision:
                 EYE_X = next_x
                 EYE_Z = next_z
                 CENTER_X = EYE_X + dir[0]
                 CENTER_Z = EYE_Z + dir[2]
                 glLoadIdentity()
-                gluLookAt(EYE_X, EYE_Y, EYE_Z, CENTER_X, CENTER_Y, CENTER_Z, UP_X, UP_Y, UP_Z)
+                gluLookAt(EYE_X, EYE_Y, EYE_Z,
+                          CENTER_X, CENTER_Y, CENTER_Z,
+                          UP_X,    UP_Y,    UP_Z)
 
-    #retroceder observador    
-    if keys[pygame.K_DOWN]: #Cambiar colisiones
-        #nueva pos
+    if keys[pygame.K_DOWN]:
         next_x = EYE_X - dir[0]
         next_z = EYE_Z - dir[2]
 
@@ -209,39 +198,43 @@ while not done:
         player.Position[2] = EYE_Z
         player.Direction[0] = dir[0]
         player.Direction[2] = dir[2]
-        
-        # Detecta la colision con el mapa
+
         if not player.Detcol():
-            # Checa colision con los cubos y el jugador
             collision = False
             for cube in cubos:
                 if cube.checkPlayerCollision([next_x, EYE_Y, next_z]):
                     collision = True
                     break
-            
             if not collision:
                 EYE_X = next_x
                 EYE_Z = next_z
                 CENTER_X = EYE_X + dir[0]
-                CENTER_Z = EYE_Z + dir[2] 
+                CENTER_Z = EYE_Z + dir[2]
                 glLoadIdentity()
-                gluLookAt(EYE_X, EYE_Y, EYE_Z, CENTER_X, CENTER_Y, CENTER_Z, UP_X, UP_Y, UP_Z)
+                gluLookAt(EYE_X, EYE_Y, EYE_Z,
+                          CENTER_X, CENTER_Y, CENTER_Z,
+                          UP_X,    UP_Y,    UP_Z)
 
     if keys[pygame.K_LEFT]:
-        theta = 1
-        total_theta += 1  # Increment total rotation
+        theta =  1
         lookat()
     if keys[pygame.K_RIGHT]:
         theta = -1
-        total_theta -= 1  # Decrement total rotation
-        lookat()                     
-    
+        lookat()
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    display()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                shoot_bullet(bullets)
 
+    update_and_collide_bullets(bullets, cubos, DimBoard)
+
+
+    display()
     pygame.display.flip()
     pygame.time.wait(10)
 
